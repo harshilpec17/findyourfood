1dfd994262a18f157c32b98d438e3ac8971a805b
harshilpec17 <harshilsuthar1995@gmail.com>
2024-02-28 14:46:27 -0500

    add: add time feature

--- src/Component/MenuComponent/MenuItemList.jsx
+++ src/Component/MenuComponent/MenuItemList.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import { MENU_FOOD_IMAGE } from "../../utils/constant";
 import { BiRupee } from "react-icons/bi";
 
 const MenuItemList = ({ items }) => {
+  const [quantity, setQuantity] = useState({});
+  const [addItem, setAddItem] = useState([]);
+  const [addToggle, setAddToggle] = useState(false);
+
+  const handleClick = (i) => {
+    setAddItem((prev) => [{ i, counter: 0 }, ...prev]);
+  };
+
+  const removeItem = (i) => {
+    setAddItem((prev) =>
+      prev.filter((dlt) => dlt.card.info.id !== Object.keys[quantity])
+    );
+  };
+
+  const itemQuantity = (items) => {
+    let counter = {};
+    if (items.length === 0) return;
+    items.forEach((q) => {
+      if (q.card.info.id === q.card.info.id) {
+        counter[q.card.info.id] = (counter[q.card.info.id] || 0) + 1;
+        setQuantity(counter);
+      }
+    });
+  };
+
+  useEffect(() => {
+    itemQuantity(addItem);
+  }, [addItem]);
+
+  console.log(quantity);
+  console.log(addItem);
+
   return (
     <div>
       {items.map((item) => (
@@ -28,10 +61,25 @@ const MenuItemList = ({ items }) => {
               alt="No Image Required"
               className="w-[118px] h-[96px] bg-cover bg-center border rounded"
             />
-            <div className="w-[118px] px-2 border rounded cursor-pointer">
-              <h1 className="text-bold text-center text-[#3D9B6D] font-semibold">
+            <div className="w-[118px] ">
+              <div className="flex justify-between items-center bg-black rounded border">
+                <p
+                  onClick={() => handleClick(item)}
+                  className="bg-black text-white font-bold px-2 py-0.5"
+                >
+                  +
+                </p>
+                <p className="bg-black text-white font-bold px-2 py-0.5">0</p>
+                <p
+                  onClick={() => removeItem(item)}
+                  className="bg-black text-white font-bold px-2 py-0.5"
+                >
+                  -
+                </p>
+              </div>
+              {/* <h1 className="text-bold border rounded cursor-pointer text-center text-[#3D9B6D] font-semibold">
                 ADD +
-              </h1>
+              </h1> */}
             </div>
           </div>
         </div>
